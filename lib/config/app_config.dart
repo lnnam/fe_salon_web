6b5430018b0f1dcc4306bdb1ef99e2ed97bf83c3
cu nam <lnnam.itwork@gmail.com>
2025-12-03 15:31:28 +0000

    Update API URL to production: apiclient.greatyarmouthnails.com

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,6 +1,5 @@
 class AppConfig {
-  // static const api_url = 'https://apiclient.greatyarmouthnails.com';
-  static const api_url = 'http://localhost:8080';
+  static const api_url = 'https://apiclient.greatyarmouthnails.com';
   static const api_url_login = '$api_url/api/auth/signin';
   //static const api_url_booking_home = '$api_url/api/booking/list';
   static const api_url_booking_staff = '$api_url/api/booking/staff';
